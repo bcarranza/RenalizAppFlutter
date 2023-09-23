ed0fd9c373610cfc53a504357601515779be1178
Davinderpro01 <d-alex01@hotmail.com>
2023-09-22 21:03:39 -0600

    ADD: form connection to database

--- lib/features/test/presentation/screens/patient_form.dart
+++ lib/features/test/presentation/screens/patient_form.dart
@@ -1,6 +1,8 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:go_router/go_router.dart';
+import 'package:http/http.dart' as http;
 
 class PatientForm extends StatefulWidget {
   final GoRouter appRouter;
@@ -14,6 +16,10 @@ class _PatientFormState extends State<PatientForm> {
   DateTime? selectedDate;
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
   final GlobalKey<NavigatorState> _navigatorKey = GlobalKey<NavigatorState>();
+  // Define los controladores para los campos de texto
+  final TextEditingController _nameController = TextEditingController();
+  final TextEditingController _addressController = TextEditingController();
+  final TextEditingController _phoneNumberController = TextEditingController();
 
   Future<void> _selectDate(BuildContext context) async {
     final DateTime picked = (await showDatePicker(
@@ -28,6 +34,35 @@ class _PatientFormState extends State<PatientForm> {
       setState(() {
         selectedDate = picked;
       });
+    }
+  }
+
+  Future<int> _httpsCall() async {
+    final user = FirebaseAuth.instance.currentUser;
+
+    final idTokenResult = await user?.getIdTokenResult();
+    final token = idTokenResult?.token;
+
+    Uri uri = Uri.parse(
+        'https://us-central1-renalizapp-dev-2023-396503.cloudfunctions.net/renalizapp-2023-dev-postRegister');
+
+    final body = {
+      'name': _nameController.text,
+      'dob': selectedDate.toString(),
+      'address': _addressController.text,
+      'phoneNumber': _phoneNumberController.text,
+      'uid': user?.uid.toString(),
+    };
+    final response = await http.post(uri, body: body);
+
+    if (response.statusCode == 201) {
+      return response.statusCode;
+    } else {
+      print(_nameController.text);
+      print(selectedDate.toString());
+      print(_addressController.text);
+      print(_phoneNumberController.text);
+      throw Exception('Error: ${response.statusCode}');
     }
   }
 
@@ -62,7 +97,7 @@ class _PatientFormState extends State<PatientForm> {
                           return 'Ingrese los nombres';
                         }
                         return null;
-                      }),
+                      }, controller: _nameController), // Asigna _nameController
                       SizedBox(height: 5),
                       InkWell(
                         onTap: () => _selectDate(context),
@@ -89,21 +124,54 @@ class _PatientFormState extends State<PatientForm> {
                           return 'Ingrese la dirección';
                         }
                         return null;
-                      }),
+                      },
+                          controller:
+                              _addressController), // Asigna _addressController
                       _buildTextField('Teléfono', (value) {
                         if (value!.isEmpty) {
                           return 'Ingrese el teléfono';
                         }
                         return null;
-                      }),
+                      },
+                          controller:
+                              _phoneNumberController), // Asigna _phoneNumberController
                     ],
                   ),
                 ),
                 SizedBox(height: 20),
                 ElevatedButton(
-                  onPressed: () {
+                  onPressed: () async {
                     if (_formKey.currentState!.validate()) {
-                      context.go('/profile');
+                      try {
+                        final responseCode = await _httpsCall();
+                        if (responseCode == 201) {
+                          // Procesa la respuesta exitosa aquí
+                          // Por ejemplo, puedes redirigir al usuario a una página de éxito
+                          // o mostrar un mensaje de éxito.
+                          widget.appRouter.go('/profile');
+                        } else {
+                          // Mostrar un mensaje de error al usuario
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            SnackBar(
+                              content: Text(
+                                'Error al enviar el formulario. Por favor, inténtelo de nuevo. Detalles del error: $responseCode',
+                              ),
+                              backgroundColor: Colors.red,
+                            ),
+                          );
+                        }
+                      } catch (e) {
+                        // Manejo de errores
+                        print('Error: $e');
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          SnackBar(
+                            content: Text(
+                              'Error al enviar el formulario. Por favor, inténtelo de nuevo.',
+                            ),
+                            backgroundColor: Colors.red,
+                          ),
+                        );
+                      }
                     }
                   },
                   style: ElevatedButton.styleFrom(
@@ -123,10 +191,14 @@ class _PatientFormState extends State<PatientForm> {
   }
 
   Widget _buildTextField(
-      String labelText, String? Function(String?)? validator) {
+    String labelText,
+    String? Function(String?)? validator, {
+    required TextEditingController controller, // Agrega el parámetro controller
+  }) {
     return Padding(
       padding: const EdgeInsets.symmetric(vertical: 5.0),
       child: TextFormField(
+        controller: controller, // Asigna el controlador al campo de texto
         validator: validator,
         style: TextStyle(
           fontSize: 16,
