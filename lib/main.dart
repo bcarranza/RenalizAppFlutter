3bed6764cf332fea2aa7b8f5a70d7ea3bae04be9
Davinderpro01 <d-alex01@hotmail.com>
2023-09-23 01:52:44 -0600

    ADD: initialice firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,15 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:renalizapp/config/config.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
+import 'firebase_options.dart';
 
 void main() async {
   await Environment.initEnvironment();
-
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MainApp());
 }
 
