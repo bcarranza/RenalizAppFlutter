8073a74645532acc233691ba078db3d63bb4bbb9
Carlos Vásquez <49454955+lighnio@users.noreply.github.com>
2023-09-30 13:26:14 -0600

    RA 60 - Filter local blogs in memory by title or category (#27)
    * ADD: Textfield for filter, not working
    
    * ADD: Filter local blogs in memory

--- lib/features/home/presentation/widgets/main_list.dart
+++ lib/features/home/presentation/widgets/main_list.dart
@@ -16,7 +16,9 @@ class MainList extends StatefulWidget {
 
 class _MainListState extends State<MainList> {
   ScrollController _scrollController = ScrollController();
+  TextEditingController _filter = TextEditingController();
   List blogs = [];
+  List filtredBlogs = [];
   int perPage = 10;
   int page = 1;
   int allItems = 0;
@@ -40,11 +42,23 @@ class _MainListState extends State<MainList> {
     if (response.statusCode == 200) {
       allItems = jsonDecode(response.body)['allItems'];
       blogs.addAll(jsonDecode(response.body)['data']);
+      filtredBlogs.addAll(jsonDecode(response.body)['data']);
       loading = false;
       return true;
     } else {
       throw Exception('Error: ${response.statusCode}');
     }
+  }
+
+  void filterData(String filtro) {
+    setState(() {
+      filtredBlogs = blogs.where((articulo) {
+        return articulo['title'].toLowerCase().contains(filtro.toLowerCase()) ||
+            articulo['category'].toLowerCase().contains(filtro.toLowerCase());
+        // articulo['tags']
+        //     .any((tag) => tag.toLowerCase().contains(filtro.toLowerCase()));
+      }).toList();
+    });
   }
 
   _loadMoreBlogs() async {
@@ -60,6 +74,7 @@ class _MainListState extends State<MainList> {
     if (response.statusCode == 200) {
       setState(() {
         blogs.addAll(jsonDecode(response.body)['data']);
+        filtredBlogs.addAll(jsonDecode(response.body)['data']);
       });
       return true;
     } else {
@@ -96,102 +111,127 @@ class _MainListState extends State<MainList> {
           //     .compareTo(a['publication_date']['_seconds']));
 
           return StatefulBuilder(
-              builder: (context, setState) => ListView.builder(
-                    itemCount: blogs.length,
-                    controller: _scrollController,
-                    itemBuilder: (context, index) {
-                      final blog = blogs[index] as Map;
-                      final tags = blog['tags'] as List;
-                      final displayedTags = tags.take(3).toList();
-                      final remainingTagsCount =
-                          tags.length - displayedTags.length;
-                      final isTruncated = remainingTagsCount > 0;
-
-                      return GestureDetector(
-                          onTap: () {
-                            blogDetail = blog;
-                            context.go('/blog-detail');
+              builder: (context, setState) => Column(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(16.0),
+                        child: TextField(
+                          onChanged: (texto) {
+                            filterData(texto);
                           },
-                          child: Card(
-                            elevation:
-                                2, // Puedes ajustar la elevación según tus preferencias
-                            child: ListTile(
-                              contentPadding: const EdgeInsets.all(
-                                  16), // Ajusta el relleno según tus necesidades
-                              leading: CircleAvatar(
-                                backgroundImage: NetworkImage(
-                                  blog['cover_image'],
-                                ),
-                              ),
-                              title: Text(
-                                blog['title'],
-                                style: const TextStyle(
-                                    fontWeight: FontWeight.bold),
-                              ),
-                              subtitle: Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  Text(blog['category']),
-                                  Text("Autor: ${blog['author']}"),
-                                  Text(DateTime.fromMicrosecondsSinceEpoch(
-                                          blog['publication_date']["_seconds"] *
-                                              1000000)
-                                      .toLocal()
-                                      .toString()
-                                      .split('.')[0]),
-                                  Text(
-                                    blog['description'],
-                                    overflow: TextOverflow.ellipsis,
-                                  ),
-                                  displayedTags.isNotEmpty
-                                      ? SizedBox(height: 20)
-                                      : Container(),
-                                  Wrap(
-                                    spacing: 6,
-                                    runSpacing: 6,
-                                    children: displayedTags
-                                        .map((tag) => Chip(
-                                            label: Padding(
-                                              padding: EdgeInsets.symmetric(
-                                                  vertical: 0.0,
-                                                  horizontal:
-                                                      2.0), // Ajusta el padding según tus preferencias
-                                              child: Text(tag),
-                                            ),
-                                            shape: RoundedRectangleBorder(
-                                              borderRadius:
-                                                  BorderRadius.circular(25.0),
-                                            ),
-                                            backgroundColor: generateColor(tag),
-                                            labelStyle: TextStyle(
-                                              color: Colors.white,
-                                            )))
-                                        .toList(),
-                                  ),
-                                  isTruncated
-                                      ? Text(
-                                          "(+$remainingTagsCount más)",
-                                          style: TextStyle(color: Colors.blue),
-                                        )
-                                      : SizedBox()
-                                ],
-                              ),
-                              trailing: IconButton(
-                                icon: Icon(
-                                  Icons.star,
-                                  color: blog['isStarred']
-                                      ? Colors.yellow
-                                      : Colors
-                                          .grey, // Color de la estrella según el valor de isEstrellado
-                                ),
-                                onPressed: () {
-                                  // Maneja la acción cuando se hace clic en la estrella aquí
-                                  // Puedes agregar lógica para cambiar el valor de isEstrellado
+                          decoration: InputDecoration(
+                            labelText: "Buscar",
+                            hintText: "Ingrese un término de búsqueda",
+                            prefixIcon: Icon(Icons.search),
+                          ),
+                        ),
+                      ),
+                      Expanded(
+                        child: ListView.builder(
+                          itemCount: filtredBlogs.length,
+                          controller: _scrollController,
+                          itemBuilder: (context, index) {
+                            final blog = filtredBlogs[index] as Map;
+                            final tags = blog['tags'] as List;
+                            final displayedTags = tags.take(3).toList();
+                            final remainingTagsCount =
+                                tags.length - displayedTags.length;
+                            final isTruncated = remainingTagsCount > 0;
+
+                            return GestureDetector(
+                                onTap: () {
+                                  blogDetail = blog;
+                                  context.go('/blog-detail');
                                 },
-                              ),
-                            ),
-                          ));
-                    },
+                                child: Card(
+                                  elevation:
+                                      2, // Puedes ajustar la elevación según tus preferencias
+                                  child: ListTile(
+                                    contentPadding: const EdgeInsets.all(
+                                        16), // Ajusta el relleno según tus necesidades
+                                    leading: CircleAvatar(
+                                      backgroundImage: NetworkImage(
+                                        blog['cover_image'],
+                                      ),
+                                    ),
+                                    title: Text(
+                                      blog['title'],
+                                      style: const TextStyle(
+                                          fontWeight: FontWeight.bold),
+                                    ),
+                                    subtitle: Column(
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      children: [
+                                        Text(blog['category']),
+                                        Text("Autor: ${blog['author']}"),
+                                        Text(
+                                            DateTime.fromMicrosecondsSinceEpoch(
+                                                    blog['publication_date']
+                                                            ["_seconds"] *
+                                                        1000000)
+                                                .toLocal()
+                                                .toString()
+                                                .split('.')[0]),
+                                        Text(
+                                          blog['description'],
+                                          overflow: TextOverflow.ellipsis,
+                                        ),
+                                        displayedTags.isNotEmpty
+                                            ? SizedBox(height: 20)
+                                            : Container(),
+                                        Wrap(
+                                          spacing: 6,
+                                          runSpacing: 6,
+                                          children: displayedTags
+                                              .map((tag) => Chip(
+                                                  label: Padding(
+                                                    padding: EdgeInsets.symmetric(
+                                                        vertical: 0.0,
+                                                        horizontal:
+                                                            2.0), // Ajusta el padding según tus preferencias
+                                                    child: Text(tag),
+                                                  ),
+                                                  shape: RoundedRectangleBorder(
+                                                    borderRadius:
+                                                        BorderRadius.circular(
+                                                            25.0),
+                                                  ),
+                                                  backgroundColor:
+                                                      generateColor(tag),
+                                                  labelStyle: TextStyle(
+                                                    color: Colors.white,
+                                                  )))
+                                              .toList(),
+                                        ),
+                                        isTruncated
+                                            ? Text(
+                                                "(+$remainingTagsCount más)",
+                                                style: TextStyle(
+                                                    color: Colors.blue),
+                                              )
+                                            : SizedBox()
+                                      ],
+                                    ),
+                                    trailing: IconButton(
+                                      icon: Icon(
+                                        Icons.star,
+                                        color: blog['isStarred']
+                                            ? Colors.yellow
+                                            : Colors
+                                                .grey, // Color de la estrella según el valor de isEstrellado
+                                      ),
+                                      onPressed: () {
+                                        // Maneja la acción cuando se hace clic en la estrella aquí
+                                        // Puedes agregar lógica para cambiar el valor de isEstrellado
+                                      },
+                                    ),
+                                  ),
+                                ));
+                          },
+                        ),
+                      ),
+                    ],
                   ));
         }
 
