5e78acc7b7c57e864bfd954cd977174eb4cdfa70
Danilo Portillo <38937343+DJPORTILLO30@users.noreply.github.com>
2023-09-29 22:05:51 -0600

    fix home (#21)

--- lib/features/home/presentation/screens/home_screen.dart
+++ lib/features/home/presentation/screens/home_screen.dart
@@ -11,9 +11,7 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Renalizapp'),
-      ),
+      appBar: CustomAppBar(),
       body: Center(child: MainList()),
     );
   }
