9277a89dca608a4a484b01b4fa974eae4922a154
RDJ20 <69997480+RDJ20@users.noreply.github.com>
2023-10-02 18:17:31 -0600

    Ra 73 fix test2 (#29)
    * Link hardcoded fixed
    
    * Now is fixed
    
    * Added the correct link
    
    * Pagina del historial otra vez funcional
    
    * AlertDialog para redirigir y bloqueo de pregunta 0
    
    * Preguntas mas bonitas
    
    * Mejorado boton de siguiente

--- lib/features/test/presentation/screens/questions_interface.dart
+++ lib/features/test/presentation/screens/questions_interface.dart
@@ -8,6 +8,8 @@ import 'package:http/http.dart' as http;
 import 'dart:async';
 import 'package:provider/provider.dart';
 import 'package:renalizapp/features/shared/infrastructure/provider/auth_provider.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
 
 class Question {
   final String question;
@@ -93,14 +95,17 @@ class QuizzPage extends StatefulWidget {
 }
 
 class _QuizzPageState extends State<QuizzPage> {
+  bool isZeroSelected = false;
   late Future<List<Question>> questionsFuture;
   int questionIndex = 0;
   double score = 0;
   List<Map<String, dynamic>> answeredQuestions = [];
 
   Future<List<Question>> fetchQuizJson() async {
+    final Uri uri = Uri.parse(dotenv.env['API_URL']! + 'getTestById');  // Utiliza dotenv aquí
+
     final response = await http.post(
-      Uri.parse('https://us-central1-renalizapp-dev-2023-396503.cloudfunctions.net/renalizapp-2023-prod-getTestById'),
+      uri,
       headers: <String, String>{
         'Content-Type': 'application/json; charset=UTF-8',
       },
@@ -108,8 +113,6 @@ class _QuizzPageState extends State<QuizzPage> {
         'id': 'test2',
       }),
     );
-
-  
 
     if (response.statusCode == 200) {
       final Map<String, dynamic> quizMap = json.decode(response.body) as Map<String, dynamic>;
@@ -171,29 +174,122 @@ class _QuizzPageState extends State<QuizzPage> {
     });
 
      if (questionIndex >= questions.length) {
+     
+
+
       DateTime now = DateTime.now();
       String formattedDate = DateFormat('yyyy-MM-dd – kk:mm').format(now);
       final response = Response(score: score, answeredQuestions: answeredQuestions, timestamp: formattedDate);
       final testResultJson = response.toJson();
       String response1 = jsonEncode(testResultJson);
       final authProvider = context.read<AuthProvider>();  // Cambio aquí
+      String riskMessage;
+      String riskDescription;
 
       String? userName = authProvider.currentUser?.displayName;
       String? uid = authProvider.currentUser?.uid;
+
       if (uid != null) {
+
+        if (score >= 0 && score <= 4) {
+            riskMessage = "Buenas prácticas para la salud renal.";
+            riskDescription = "Sigue cuidando tus riñones.";
+          } else if (score >= 5 && score <= 10) {
+            riskMessage = "Riesgo moderado.";
+            riskDescription = "Considera hablar con un médico para una evaluación.";
+          } else {
+            riskMessage = "Riesgo alto.";
+            riskDescription =
+                "Se recomienda consultar a un médico para una evaluación y consejo médico.";
+          }
         _postTestResult(response1, uid);
-      } else {
-        _saveTestResult(response1);
+        _saveToHistory(riskMessage, riskDescription);
+        showRedirectDialog(context,score,riskMessage, riskDescription);
+
+      } 
+      
+      else {
+          
+          if (score >= 0 && score <= 4) {
+            riskMessage = "Buenas prácticas para la salud renal.";
+            riskDescription = "Sigue cuidando tus riñones.";
+          } else if (score >= 5 && score <= 10) {
+            riskMessage = "Riesgo moderado.";
+            riskDescription = "Considera hablar con un médico para una evaluación.";
+          } else {
+            riskMessage = "Riesgo alto.";
+            riskDescription =
+                "Se recomienda consultar a un médico para una evaluación y consejo médico.";
+          }
+
+
+          _saveTestResult(response1);
+          _saveToHistory(riskMessage, riskDescription);  // Llamada al nuevo método aquí
+          showRedirectDialog(context,score,riskMessage, riskDescription);
       }
+      
+      
     }
   }
 
+
+  
+  void navigateToLogin(BuildContext context) {
+    final goRouter = GoRouter.of(context);
+    goRouter.go('/test');
+  }
+
+  Future<void> showRedirectDialog(BuildContext context, double puntuacion, String mensaje, String descripcion) {
+    String score = puntuacion.toString();
+  return showDialog<void>(
+    context: context,
+    barrierDismissible: false, // user must tap button to close dialog
+    builder: (BuildContext context) {
+      return AlertDialog(
+        title: Text('Puntuación: '+score),
+        content:Column(
+          mainAxisSize: MainAxisSize.min,  // para tomar el espacio mínimo necesario
+          children: [
+            Text(
+              mensaje,
+              style: TextStyle(
+                fontSize: 18.0,  // tamaño de la fuente
+                fontFamily: 'Roboto',  // fuente
+                // y otros estilos que desees
+              ),
+            ),
+            SizedBox(height: 20),  // espacio entre los mensajes
+            Text(
+              descripcion,
+              style: TextStyle(
+                fontSize: 18.0,  // tamaño de la fuente
+                fontFamily: 'Roboto',  // fuente
+                // y otros estilos que desees
+              ),
+            ),
+          ],
+        ),
+        actions: <Widget>[
+          TextButton(
+            child: Text('Redirigir'),
+            onPressed: () {
+              Navigator.of(context).pop();  // Cerrar el diálogo
+              navigateToLogin(context);  // Navegar al login
+            },
+          ),
+        ],
+      );
+    },
+  );
+}
+
+
   // Nueva función para realizar la solicitud POST
   Future<void> _postTestResult(String testResultJson, String uid) async {
-    final url = 'https://us-central1-renalizapp-dev-2023-396503.cloudfunctions.net/renalizapp-2023-prod-postTestResults';
+    final Uri uri = Uri.parse(dotenv.env['API_URL']! + 'postTestResults');  // Utiliza dotenv aquí
 
     final response = await http.post(
-      Uri.parse(url),
+      uri,
       headers: <String, String>{
         'Content-Type': 'application/json; charset=UTF-8',
       },
@@ -208,7 +304,31 @@ class _QuizzPageState extends State<QuizzPage> {
     } else {
       print('Failed to post test result: ${response.statusCode}');
     }
+}
+
+
+Future<void> _saveToHistory(String riskMessage, String riskDescription) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+
+    DateTime now = DateTime.now();
+    String formattedDate = DateFormat('yyyy-MM-dd – kk:mm').format(now);
+
+    String newResult = "$score;$riskMessage;$riskDescription;$formattedDate";
+
+    List<String>? existingResults = prefs.getStringList('history');
+
+    if (existingResults == null) {
+      existingResults = [newResult];
+    } else {
+      existingResults.add(newResult);
+    }
+
+    await prefs.setStringList('history', existingResults);
+    print('Historial de tests para USUARIO: $existingResults');
   }
+
+
+  
   // Función para guardar el resultado del test en SharedPreferences
  Future<void> _saveTestResult(String testResultJson) async {
   SharedPreferences prefs = await SharedPreferences.getInstance();
@@ -231,85 +351,133 @@ class _QuizzPageState extends State<QuizzPage> {
   print('Historial de tests guardados: $existingTestResults');
 }
   @override
-  Widget build(BuildContext context) {
-    return FutureBuilder<List<Question>>(
-      future: questionsFuture,
-      builder: (context, snapshot) {
-        if (snapshot.connectionState == ConnectionState.waiting) {
+Widget build(BuildContext context) {
+  final size = MediaQuery.of(context).size;
+  final double maxWidth = 1200.0;
+
+  return FutureBuilder<List<Question>>(
+    future: questionsFuture,
+    builder: (context, snapshot) {
+      if (snapshot.connectionState == ConnectionState.waiting) {
+        return Scaffold(
+          appBar: AppBar(
+            title: Text('Cargando...'),
+          ),
+          body: Center(
+            child: CircularProgressIndicator(),
+          ),
+        );
+      } else if (snapshot.hasError) {
+        return Scaffold(
+          appBar: AppBar(
+            title: Text('Error'),
+          ),
+          body: Center(
+            child: Text('Error: ${snapshot.error}'),
+          ),
+        );
+      } else {
+        final questions = snapshot.data!;
+
+        if (questionIndex >= questions.length) {
           return Scaffold(
             appBar: AppBar(
-              title: Text('Cargando...'),
+              title: Text('Cuestionario Completado'),
             ),
             body: Center(
-              child: CircularProgressIndicator(),
-            ),
-          );
-        } else if (snapshot.hasError) {
-          return Scaffold(
-            appBar: AppBar(
-              title: Text('Error'),
-            ),
-            body: Center(
-              child: Text('Error: ${snapshot.error}'),
-            ),
-          );
-        } else {
-          final questions = snapshot.data!;
-
-          if (questionIndex >= questions.length) {
-            return Scaffold(
-              appBar: AppBar(
-                title: Text('Cuestionario Completado'),
-              ),
-              body: Center(
-                child: Text('Puntuación: $score'),
-              ),
-            );
-          }
-
-          return Scaffold(
-            appBar: AppBar(
-              title: Text('Cuestionario'),
-            ),
-            body: Column(
-              children: [
-                Text(
-                  questions[questionIndex].question,
-                  style: TextStyle(fontSize: 28),
-                ),
-                ...questions[questionIndex].answers.map<Widget>((answer) {
-                  return questions[questionIndex].type == 'multiple'
-                      ? CheckboxListTile(
-                          title: Text(answer.text),
-                          value: answer.isSelected,
-                          onChanged: (bool? value) {
-                            setState(() {
-                              answer.isSelected = value!;
-                            });
-                          },
-                        )
-                      : RadioListTile<int>(
-                          title: Text(answer.text),
-                          value: answer.value,
-                          groupValue: questions[questionIndex].answers.indexWhere((answer) => answer.isSelected),
-                          onChanged: (int? value) {
-                            setState(() {
-                              for (var ans in questions[questionIndex].answers) {
-                                ans.isSelected = ans.value == value;
-                              }
-                            });
-                          },
-                        );
-                }).toList(),
-                ElevatedButton(
-                  onPressed: () => answerQuestion(questions),
-                  child: Text('Siguiente'),
-                ),
-              ],
+              child: Text('Puntuación: $score'),
             ),
           );
         }
-      },
-    );
-  }
+
+        var isAnswerSelected = questions[questionIndex].answers.any((answer) => answer.isSelected);
+
+        return Scaffold(
+          appBar: AppBar(
+            title: Text('Cuestionario'),
+          ),
+          body: Center(
+            child: ConstrainedBox(
+              constraints: BoxConstraints(maxWidth: maxWidth),
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                      width: size.width,
+                      padding: EdgeInsets.all(16.0),
+                      margin: EdgeInsets.only(bottom: 16.0),
+                      decoration: BoxDecoration(
+                        color: Colors.blue,
+                        borderRadius: BorderRadius.circular(8.0),
+                      ),
+                      child: Text(
+                        questions[questionIndex].question,
+                        style: TextStyle(fontSize: 28, color: Colors.white),
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                    ...questions[questionIndex].answers.map<Widget>((answer) {
+                      return questions[questionIndex].type == 'multiple'
+                          ? CheckboxListTile(
+                              title: Text(answer.text),
+                              value: answer.isSelected,
+                              onChanged: (answer.value == 0 || !isZeroSelected)
+                                  ? (bool? value) {
+                                      setState(() {
+                                        answer.isSelected = value!;
+                                        if (answer.value == 0) {
+                                          isZeroSelected = value;
+                                        }
+                                        for (var otherAnswer in questions[questionIndex].answers) {
+                                          if (otherAnswer.value != 0) {
+                                            if (isZeroSelected) {
+                                              otherAnswer.isSelected = false;
+                                            }
+                                          }
+                                        }
+                                      });
+                                    }
+                                  : null,
+                            )
+                          : RadioListTile<int>(
+                              title: Text(answer.text),
+                              value: answer.value,
+                              groupValue: questions[questionIndex].answers.indexWhere((answer) => answer.isSelected),
+                              onChanged: (int? value) {
+                                setState(() {
+                                  for (var ans in questions[questionIndex].answers) {
+                                    ans.isSelected = ans.value == value;
+                                  }
+                                });
+                              },
+                            );
+                    }).toList(),
+                    SizedBox(
+                      width: double.infinity,
+                      child: ElevatedButton(
+                        onPressed: isAnswerSelected ? () => answerQuestion(questions) : null,
+                        child: Text('Siguiente'),
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor : Colors.blue,
+                          foregroundColor : Colors.white,
+                          padding: EdgeInsets.symmetric(vertical: 16),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        );
+      }
+    },
+  );
+}
+
+
+
+  
 }
