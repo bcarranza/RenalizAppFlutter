79e73ebe16b206b514bd8380e3fe48081933149f
RDJ20 <69997480+RDJ20@users.noreply.github.com>
2023-10-06 03:41:00 -0600

    Ra 68 bulk info (#33)
    * Para Bulk
    
    * Ya consume la api para los test en la bd
    
    * merge fix
    
    * Arreglos y comentarios de prints
    
    ---------
    
    Co-authored-by: Danilo Portillo <38937343+DJPORTILLO30@users.noreply.github.com>

--- lib/features/test/presentation/screens/history_page.dart
+++ lib/features/test/presentation/screens/history_page.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:go_router/go_router.dart';
-import 'package:intl/intl.dart';
+import 'package:http/http.dart' as http;
+import 'package:provider/provider.dart';
+import 'package:renalizapp/features/shared/infrastructure/provider/auth_provider.dart';
+import 'dart:convert';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class HistoryPage extends StatefulWidget {
   final GoRouter appRouter;
@@ -14,23 +18,63 @@ class HistoryPage extends StatefulWidget {
 
 class _HistoryPageState extends State<HistoryPage> {
   List<String> history = [];
+  bool _isLoading = true;  // Variable para rastrear si los datos están cargando
 
   // Carga el historial desde SharedPreferences
   Future<void> loadHistory() async {
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-    List<String>? savedHistory = prefs.getStringList('history');
-    if (savedHistory != null) {
-      setState(() {
-        history = savedHistory;
-      });
+    final authProvider = context.read<AuthProvider>();
+    String? uid = authProvider.currentUser?.uid;
+    //print(uid);
+
+    if (uid != null) {
+      // Usuario ha iniciado sesión
+      final Uri uri = Uri.parse(dotenv.env['API_URL']! + 'getTestsByUid');
+      final response = await http.post(
+        uri,
+        body: json.encode({'uid': uid}),
+        headers: {"Content-Type": "application/json"},
+      );
+
+      if (response.statusCode == 200) {
+        final Map<String, dynamic> data = json.decode(response.body);
+        final String formattedResult = data['formattedResult'];
+        //print(formattedResult);
+
+        // Dividir formattedResult en una lista de cadenas
+        final List<String> historyItems = formattedResult.split(', ');
+
+        SharedPreferences prefs = await SharedPreferences.getInstance();
+        await prefs.setStringList('history', historyItems);
+
+        setState(() {
+          history = historyItems;
+          _isLoading = false;  // Datos cargados, establecer _isLoading a false
+        });
+        //print(prefs);
+      } else {
+        // Manejar error en la respuesta
+        setState(() {
+          _isLoading = false;  // Error en la respuesta, establecer _isLoading a false
+        });
+      }
+    } else {
+      //print("NO se ha iniciado sesion");
+      // Usuario no ha iniciado sesión
+      SharedPreferences prefs = await SharedPreferences.getInstance();
+      List<String>? savedHistory = prefs.getStringList('history');
+      if (savedHistory != null) {
+        setState(() {
+          history = savedHistory;
+          _isLoading = false;  // Datos cargados, establecer _isLoading a false
+        });
+      }
     }
-    //print('Historial cargado: $history'); // Agregar este print
   }
 
   @override
   void initState() {
     super.initState();
-    loadHistory(); // Carga el historial cuando se inicia la página
+    loadHistory();  // Carga el historial cuando se inicia la página
   }
 
   @override
@@ -41,75 +85,70 @@ class _HistoryPageState extends State<HistoryPage> {
         backgroundColor: Colors.blue,
         foregroundColor: Colors.white,
         leading: IconButton(
-          icon: Icon(Icons.arrow_back), // Icono de flecha hacia atrás
+          icon: Icon(Icons.arrow_back),  // Icono de flecha hacia atrás
           onPressed: () {
             // Navega de regreso a la pantalla anterior
             widget.appRouter.pop();
           },
         ),
       ),
-      body: ListView.builder(
-        itemCount: history.length,
-        itemBuilder: (context, index) {
-          // Divide la cadena almacenada en SharedPreferences
-          final data = history[index].split(';');
-          if (data.length == 4) {
-            final score = data[0];
-            final resultMessage = data[1];
-            final resultDescription = data[2];
-            final testDate = data[3];
+      body: _isLoading
+          ? Center(child: CircularProgressIndicator())  // Muestra el indicador de carga si _isLoading es true
+          : ListView.builder(
+              itemCount: history.length,
+              itemBuilder: (context, index) {
+                // Divide la cadena almacenada en SharedPreferences
+                final data = history[index].split(';');
+                if (data.length == 4) {
+                  final score = data[0];
+                  final resultMessage = data[1];
+                  final resultDescription = data[2];
+                  final testDate = data[3];
 
-            return Card(
-              elevation: 3, // Sombra ligera
-              margin: EdgeInsets.all(8), // Margen alrededor de cada tarjeta
-              child: ListTile(
-                title: Text(
-                  "Puntuación: $score",
-                  style: TextStyle(
-                    fontSize: 16, // Tamaño de fuente personalizado
-                    fontWeight: FontWeight.bold, // Tipo de letra en negrita
-                    // Cambia el tipo de letra aquí (usa una fuente personalizada o una predeterminada)
-                    fontFamily: 'Roboto', // Cambia esto según tus preferencias
-                  ),
-                ),
-                subtitle: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      "Resultado: $resultMessage",
-                      style: TextStyle(
-                        fontSize: 14, // Tamaño de fuente personalizado
-                        // Cambia el tipo de letra aquí (usa una fuente personalizada o una predeterminada)
-                        fontFamily:
-                            'Roboto', // Cambia esto según tus preferencias
+                  return Card(
+                    elevation: 3,  // Sombra ligera
+                    margin: EdgeInsets.all(8),  // Margen alrededor de cada tarjeta
+                    child: ListTile(
+                      title: Text(
+                        "Puntuación: $score",
+                        style: TextStyle(
+                          fontSize: 16,  // Tamaño de fuente personalizado
+                          fontWeight: FontWeight.bold,  // Tipo de letra en negrita
+                          fontFamily: 'Roboto',  // Cambia esto según tus preferencias
+                        ),
+                      ),
+                      subtitle: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            "Resultado: $resultMessage",
+                            style: TextStyle(
+                              fontSize: 14,  // Tamaño de fuente personalizado
+                              fontFamily: 'Roboto',  // Cambia esto según tus preferencias
+                            ),
+                          ),
+                          Text(
+                            "Descripción: $resultDescription",
+                            style: TextStyle(
+                              fontSize: 14,  // Tamaño de fuente personalizado
+                              fontFamily: 'Roboto',  // Cambia esto según tus preferencias
+                            ),
+                          ),
+                          Text(
+                            "Fecha de la prueba: $testDate",
+                            style: TextStyle(
+                              fontSize: 14,  // Tamaño de fuente personalizado
+                              fontFamily: 'Roboto',  // Cambia esto según tus preferencias
+                            ),
+                          ),
+                        ],
                       ),
                     ),
-                    Text(
-                      "Descripción: $resultDescription",
-                      style: TextStyle(
-                        fontSize: 14, // Tamaño de fuente personalizado
-                        // Cambia el tipo de letra aquí (usa una fuente personalizada o una predeterminada)
-                        fontFamily:
-                            'Roboto', // Cambia esto según tus preferencias
-                      ),
-                    ),
-                    Text(
-                      "Fecha de la prueba: $testDate",
-                      style: TextStyle(
-                        fontSize: 14, // Tamaño de fuente personalizado
-                        // Cambia el tipo de letra aquí (usa una fuente personalizada o una predeterminada)
-                        fontFamily:
-                            'Roboto', // Cambia esto según tus preferencias
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            );
-          }
-          return Container(); // Opcional: Manejar datos incorrectos
-        },
-      ),
+                  );
+                }
+                return Container();  // Opcional: Manejar datos incorrectos
+              },
+            ),
     );
   }
 }
--- lib/features/test/presentation/screens/perfil_file.dart
+++ lib/features/test/presentation/screens/perfil_file.dart
@@ -1,15 +1,14 @@
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:google_sign_in/google_sign_in.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:provider/provider.dart';
 import 'package:renalizapp/features/shared/infrastructure/provider/auth_provider.dart';
 import 'package:renalizapp/features/shared/widgets/navigation/appBar/custom_app_bar.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
-import 'package:provider/provider.dart';
-import 'package:renalizapp/features/shared/infrastructure/provider/auth_provider.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+
+
 
 class PerfilFile extends StatefulWidget {
   @override
@@ -18,13 +17,13 @@ class PerfilFile extends StatefulWidget {
 
 class _PerfilFileState extends State<PerfilFile> {
   Future<Map<String, dynamic>> getUserData(String uid) async {
-    final Uri uri = Uri.parse(dotenv.env['API_URL']! + 'getUserByUid');
+    final Uri url = Uri.parse(dotenv.env['API_URL']! + 'getUserByUid');
 
     final body = {
       "uid": uid,
     };
 
-    final response = await http.post(uri, body: body);
+    final response = await http.post(url, body: body);
 
     if (response.statusCode == 200) {
       final Map<String, dynamic> userData = json.decode(response.body);
@@ -34,11 +33,71 @@ class _PerfilFileState extends State<PerfilFile> {
     }
   }
 
+
+
+
+Future<void> postStoredTests() async {
+  // Obtener la instancia de SharedPreferences
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+
+  // Obtener la lista de tests almacenados
+  List<String>? storedTests = prefs.getStringList('historyTests');
+  //print(storedTests);
+  // Verificar si hay tests almacenados
+  if (storedTests == null || storedTests.isEmpty) {
+    print('No hay tests almacenados en el caché.');
+    return;  // Terminar la función si no hay tests almacenados
+  }
+
+  // Obtener el uid del usuario actual
+  final authProvider = context.read<AuthProvider>();
+  String? uid = authProvider.currentUser?.uid;
+  if (uid == null) {
+    print('No se pudo obtener el uid del usuario.');
+    return;  // Terminar la función si no se pudo obtener el uid
+  }
+
+  // Crear la URI para la solicitud POST
+  final Uri uri = Uri.parse(dotenv.env['API_URL']! + 'postTestResults');
+
+  for (int i = 0; i < storedTests.length; i++) {
+    // Decodificar el test actual a un objeto Dart
+    var testResult = jsonDecode(storedTests[i]);
+
+    // Enviar la solicitud POST
+    final response = await http.post(
+      uri,
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+      body: jsonEncode(<String, dynamic>{
+        'uid': uid,
+        'testResult': testResult,
+      }),
+    );
+
+    // Verificar la respuesta
+    if (response.statusCode == 200) {
+      print('Test result posted successfully: ${response.body}');
+      // Eliminar el test posteado exitosamente de la lista
+      storedTests.removeAt(i);
+      // Decrementar el índice ya que acabamos de eliminar un elemento
+      i--;
+    } else {
+      print('Failed to post test result: ${response.statusCode}');
+    }
+  }
+
+  // Guardar la lista modificada de nuevo en SharedPreferences
+  await prefs.setStringList('historyTests', storedTests);
+}
+
+
+
   @override
   Widget build(BuildContext context) {
     final authProvider = Provider.of<AuthProvider>(context);
 
-    String? userName = authProvider.currentUser?.displayName;
     String? userPhoto = authProvider.currentUser?.photoURL;
 
     ImageProvider<Object>? backgroundImage;
@@ -89,6 +148,9 @@ class _PerfilFileState extends State<PerfilFile> {
                       } else {
                         final userData = snapshot.data as Map<String, dynamic>;
                         final user = userData['user'] as Map<String, dynamic>;
+
+                        
+                        postStoredTests();
                         //print(user);
                         // Verificar si los campos son nulos antes de acceder a ellos
                         String userFirstName = user['First_Name'] ?? '';
@@ -171,411 +233,3 @@ class _PerfilFileState extends State<PerfilFile> {
     );
   }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
