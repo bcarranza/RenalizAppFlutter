7a8754781c737a4a5269ae938d9f05a89078e9ef
Carlos Vásquez <49454955+lighnio@users.noreply.github.com>
2023-10-07 08:52:16 -0600

    RA 90 - Better style for blog details (#40)

--- lib/features/home/presentation/screens/blog_detail.dart
+++ lib/features/home/presentation/screens/blog_detail.dart
@@ -2,7 +2,6 @@ import 'dart:math';
 
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
-import 'package:renalizapp/config/config.dart';
 import 'package:renalizapp/features/home/presentation/widgets/widgets.dart';
 
 class BlogDetail extends StatefulWidget {
@@ -18,27 +17,37 @@ class _BlogDetailState extends State<BlogDetail> {
   @override
   Widget build(BuildContext context) {
     List<Widget> images = [];
+    double txtScale = MediaQuery.of(context).textScaleFactor;
     blogDetail['images'].forEach((url) {
-      images.add(Image.network(url, fit: BoxFit.cover,
-          errorBuilder: (context, error, stackTrace) {
-        return Container(
-          alignment: Alignment.center,
-          child: const Text(
-            'Error loading image!',
-            style: TextStyle(fontSize: 30),
+      images.add(Container(
+        width: MediaQuery.of(context).size.width,
+        child: Center(
+          child: Container(
+            width: MediaQuery.of(context).size.width * 0.7,
+            child: Image.network(url, fit: BoxFit.cover,
+                errorBuilder: (context, error, stackTrace) {
+              return Container(
+                alignment: Alignment.center,
+                child: const Text(
+                  'Error loading image!',
+                  style: TextStyle(fontSize: 30),
+                ),
+              );
+            }),
           ),
-        );
-      }));
+        ),
+      ));
       images.add(SizedBox(
         height: 8,
       ));
     });
+
     return Scaffold(
       appBar: AppBar(
         title: Text(
           blogDetail["title"],
           style: TextStyle(
-              fontSize: 24.0,
+              fontSize: 30.0 * txtScale,
               fontWeight: FontWeight.bold,
               overflow: TextOverflow.ellipsis),
         ),
@@ -47,36 +56,39 @@ class _BlogDetailState extends State<BlogDetail> {
       body: SingleChildScrollView(
         padding: EdgeInsets.all(16.0),
         child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
             Text(
               "Fecha de Publicación: ${DateTime.fromMicrosecondsSinceEpoch(blogDetail['publication_date']["_seconds"] * 1000000).toLocal().toString().split('.')[0]}",
-              style: TextStyle(fontSize: 14.0, color: Colors.grey),
+              style: TextStyle(fontSize: 20.0 * txtScale, color: Colors.grey),
             ),
-            SizedBox(height: 10),
+            SizedBox(height: 16.0),
             Text(
-              blogDetail["description"],
-              textAlign: TextAlign.justify,
+              "Categoría: ${blogDetail["category"]}",
               style: TextStyle(
-                fontSize: 16.0,
+                  fontSize: 20.0 * txtScale, fontWeight: FontWeight.w600),
+            ),
+            SizedBox(height: 16.0),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(20, 15, 20, 15),
+              child: Text(
+                blogDetail["description"],
+                textAlign: TextAlign.justify,
+                style: TextStyle(fontSize: 24.0 * txtScale, height: 2),
               ),
             ),
             SizedBox(height: 16.0),
             ...images,
             Text(
               "Autor: ${blogDetail["author"]}",
-              style: TextStyle(fontSize: 16.0),
-            ),
-            SizedBox(height: 8.0),
-            Text(
-              "Categoría: ${blogDetail["category"]}",
-              style: TextStyle(fontSize: 16.0),
+              style: TextStyle(fontSize: 16.0 * txtScale, color: Colors.grey),
             ),
             SizedBox(height: 16.0),
             Text(
               "Etiquetas:",
-              style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
+              style: TextStyle(
+                  fontSize: 20.0 * txtScale, fontWeight: FontWeight.bold),
             ),
             SizedBox(height: 8.0),
             blogDetail["tags"].isNotEmpty
