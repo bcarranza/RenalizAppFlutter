0003d9e670ed525648f860f86cbd959451a67187
Danilo Portillo <38937343+DJPORTILLO30@users.noreply.github.com>
2023-10-13 13:07:06 -0600

    fix duplicate cards

--- lib/features/places/presentation/widgets/place_card.dart
+++ lib/features/places/presentation/widgets/place_card.dart
@@ -1,20 +1,18 @@
-
-
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:maps_launcher/maps_launcher.dart';
 import 'package:renalizapp/features/places/domain/domain.dart';
+import 'package:flutter/foundation.dart' show kIsWeb;
 
-import 'inner_shadow.dart';
+import 'inner_shadow.dart'; // Importar kIsWeb
 
 class PlaceCard extends StatelessWidget {
-
   final Place place;
 
-
-  const PlaceCard({super.key, 
+  const PlaceCard({
+    Key? key, // Añadir Key
     required this.place,
-  });
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -32,83 +30,141 @@ class PlaceCard extends StatelessWidget {
             child: GestureDetector(
               onTap: () {
                 // Acción a realizar cuando se presiona la Sección 1
-                 final params = {
-                'uid': place.uid,
-                    };
-               context.goNamed("PlaceDetail",pathParameters: params);
+                final params = {
+                  'uid': place.uid,
+                };
+                context.goNamed("PlaceDetail", pathParameters: params);
               },
-              child: InnerShadow( // Envuelve el contenedor interno con InnerShadow
-                blur: 10,
-                color: Colors.black38,
-                offset: const Offset(10, 10),
-                child: Container(
-                  height: isMobile ? 100 : 200,
-                  decoration: BoxDecoration(
-                    borderRadius: const BorderRadius.only(
-                      topLeft: Radius.circular(10.0),
-                      bottomLeft: Radius.circular(10.0),
-                    ),
-                    image: DecorationImage(
-                      image: NetworkImage(place.photoUrl),
-                      fit: BoxFit.cover,
-                    ),
-                  ),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.all(10.0),
-                        child: Text(
-                          place.nombre,
-                           maxLines: 1, 
-                          overflow: TextOverflow.ellipsis,
-                          style: TextStyle(
-                            fontSize: isMobile ? 18 : 38,
-                            fontWeight: FontWeight.bold,
-                           shadows: const <Shadow>[
-                            Shadow(
-                              offset: Offset(0.0, 0.0),
-                              blurRadius: 15.0, 
-                              color: Colors.black38,
-                            ),
-                          ],
-                          color: Colors.white.withOpacity(0.95),
-                          ),
+             child: kIsWeb && isMobile // Comprobar si se está ejecutando en un navegador
+                  ? Container(
+                      height: isMobile ? 100 : 200,
+                      decoration: BoxDecoration(
+                        borderRadius: const BorderRadius.only(
+                          topLeft: Radius.circular(10.0),
+                          bottomLeft: Radius.circular(10.0),
+                        ),
+                        image: DecorationImage(
+                          image: NetworkImage(place.photoUrl),
+                          fit: BoxFit.cover,
                         ),
                       ),
-                      Padding(
-                        padding: const EdgeInsets.all(10.0),
-                        child: Text(
-                          place.descripcion,
-                          maxLines: 1, 
-                          overflow: TextOverflow.ellipsis,
-                          style: TextStyle(
-                            fontSize: isMobile ? 12 : 24,
-                           shadows: const <Shadow>[
-                            Shadow(
-                              offset: Offset(0.0, 0.0),
-                              blurRadius: 15.0, 
-                              color: Colors.black38,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.all(10.0),
+                            child: Text(
+                              place.nombre,
+                              maxLines: 1,
+                              overflow: TextOverflow.ellipsis,
+                              style: TextStyle(
+                                fontSize: isMobile ? 18 : 38,
+                                fontWeight: FontWeight.bold,
+                                shadows: const <Shadow>[
+                                  Shadow(
+                                    offset: Offset(0.0, 0.0),
+                                    blurRadius: 15.0,
+                                    color: Colors.black38,
+                                  ),
+                                ],
+                                color: Colors.white.withOpacity(0.95),
+                              ),
                             ),
-                          ],
-                          color: Colors.white.withOpacity(0.95),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.all(10.0),
+                            child: Text(
+                              place.descripcion,
+                              maxLines: 1,
+                              overflow: TextOverflow.ellipsis,
+                              style: TextStyle(
+                                fontSize: isMobile ? 12 : 24,
+                                shadows: const <Shadow>[
+                                  Shadow(
+                                    offset: Offset(0.0, 0.0),
+                                    blurRadius: 15.0,
+                                    color: Colors.black38,
+                                  ),
+                                ],
+                                color: Colors.white.withOpacity(0.95),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    )
+                  : InnerShadow(
+                      blur: 10,
+                      color: Colors.black38,
+                      offset: const Offset(10, 10),
+                      child: Container(
+                        height: isMobile ? 100 : 200,
+                        decoration: BoxDecoration(
+                          borderRadius: const BorderRadius.only(
+                            topLeft: Radius.circular(10.0),
+                            bottomLeft: Radius.circular(10.0),
+                          ),
+                          image: DecorationImage(
+                            image: NetworkImage(place.photoUrl),
+                            fit: BoxFit.cover,
                           ),
                         ),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.all(10.0),
+                              child: Text(
+                                place.nombre,
+                                maxLines: 1,
+                                overflow: TextOverflow.ellipsis,
+                                style: TextStyle(
+                                  fontSize: isMobile ? 18 : 38,
+                                  fontWeight: FontWeight.bold,
+                                  shadows: const <Shadow>[
+                                    Shadow(
+                                      offset: Offset(0.0, 0.0),
+                                      blurRadius: 15.0,
+                                      color: Colors.black38,
+                                    ),
+                                  ],
+                                  color: Colors.white.withOpacity(0.95),
+                                ),
+                              ),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.all(10.0),
+                              child: Text(
+                                place.descripcion,
+                                maxLines: 1,
+                                overflow: TextOverflow.ellipsis,
+                                style: TextStyle(
+                                  fontSize: isMobile ? 12 : 24,
+                                  shadows: const <Shadow>[
+                                    Shadow(
+                                      offset: Offset(0.0, 0.0),
+                                      blurRadius: 15.0,
+                                      color: Colors.black38,
+                                    ),
+                                  ],
+                                  color: Colors.white.withOpacity(0.95),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
                       ),
-                    ],
-                  ),
-                ),
-              ),
+                    ),
             ),
           ),
           GestureDetector(
             onTap: () {
-             MapsLauncher.launchCoordinates(place.latitud, place.longitud);
+              MapsLauncher.launchCoordinates(place.latitud, place.longitud);
             },
             child: Container(
               height: isMobile ? 100 : 200,
               width: isMobile ? 50 : 75,
-              decoration:  BoxDecoration(
+              decoration: BoxDecoration(
                 borderRadius: const BorderRadius.only(
                   topRight: Radius.circular(10.0),
                   bottomRight: Radius.circular(10.0),
@@ -125,7 +181,6 @@ class PlaceCard extends StatelessWidget {
                     size: isMobile ? 24 : 32,
                   ),
                   const SizedBox(height: 8),
-                
                 ],
               ),
             ),
